1d65358750d31a7a73ab6363c7bb6c1ba047ddb8
Nyxfor13days <nyxfor13days@gmail.com>
2023-09-03 23:58:40 +0530

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,22 @@
 "use client";
 
 import React from "react";
+import Link from "next/link";
+import { Montserrat } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const headingFont = Montserrat({ weight: "600", subsets: ["latin"] });
 
 export default function Sidebar() {
   return (
     <div className="py-4 h-full flex flex-col space-y-4 bg-background">
-      Sidebar
+      <div className="px-3 py-2 flex-1">
+        <Link href="/dashboard" className="flex items-center mb-14">
+          <h1 className={cn(["text-xl font-bold", headingFont.className])}>
+            Schoolytics
+          </h1>
+        </Link>
+      </div>
     </div>
   );
 }
