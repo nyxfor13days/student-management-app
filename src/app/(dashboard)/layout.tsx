f997a957e940d00623f372720a6c5b27eeda39bc
Nyxfor13days <nyxfor13days@gmail.com>
2023-09-03 23:25:56 +0530

    Update layout.tsx

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -6,5 +6,14 @@ interface Props {
 }
 
 export default function DashboardLayout({ children }: Props) {
-  return <Providers>{children}</Providers>;
+  return (
+    <Providers>
+      <div className="h-full relative">
+        <div className="max-md:hidden fixed inset-y-0 h-full w-72 flex flex-col z-[80] bg-background">
+          Sidebar
+        </div>
+        <main className="md:pl-72">{children}</main>
+      </div>
+    </Providers>
+  );
 }
