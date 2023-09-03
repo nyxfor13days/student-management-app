364aeb1d27182b56bfed0055f1b32e2d2ad47755
Nyxfor13days <nyxfor13days@gmail.com>
2023-09-03 23:44:45 +0530

    Update layout.tsx

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Providers from "@/components/Providers";
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 
 interface Props {
   children: React.ReactNode;
@@ -10,9 +12,12 @@ export default function DashboardLayout({ children }: Props) {
     <Providers>
       <div className="h-full relative">
         <div className="max-md:hidden fixed inset-y-0 h-full w-72 flex flex-col z-[80] bg-background">
-          Sidebar
+          <Sidebar />
         </div>
-        <main className="md:pl-72">{children}</main>
+        <main className="md:pl-72">
+          <Navbar />
+          {children}
+        </main>
       </div>
     </Providers>
   );
