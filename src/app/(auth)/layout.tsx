4e01e6b2332dd01af8790eb35e8487dc32ed13e4
Nyxfor13days <nyxfor13days@gmail.com>
2023-09-03 22:31:37 +0530

    Created Layout for Auth Routes

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function AuthLayout({ children }: Props) {
+  return (
+    <div className="h-full flex items-center justify-center">{children}</div>
+  );
+}
