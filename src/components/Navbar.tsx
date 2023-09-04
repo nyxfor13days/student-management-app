7994bbc3da03ed5e60ac802d6043fc1ff93e0031
Nyxfor13days <nyxfor13days@gmail.com>
2023-09-04 10:44:29 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,11 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
-import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { UserButton } from "@clerk/nextjs";
+import MobileSidebar from "@/components/MobileSidebar";
 
 export default function Navbar() {
   return (
     <div className="flex items-center p-4">
-      <Button variant="ghost" size="icon" className="md:hidden">
-        <HamburgerMenuIcon />
-      </Button>
-
+      <MobileSidebar />
       <div className="flex w-full justify-end">
         <UserButton afterSignOutUrl="/" />
       </div>
