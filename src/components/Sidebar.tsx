59045d213a0efb92db5c8afb7198062077d3f57b
Nyxfor13days <nyxfor13days@gmail.com>
2023-09-04 10:48:34 +0530

    Added Active Route Indicator

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { Montserrat } from "next/font/google";
 import { cn } from "@/lib/utils";
 import {
@@ -44,6 +45,8 @@ const routes = [
 ];
 
 export default function Sidebar() {
+  const pathname = usePathname();
+
   return (
     <div className="py-8 h-full flex flex-col space-y-4 bg-primary text-primary-foreground">
       <div className="px-3 h-full flex flex-col justify-between gap-4">
@@ -58,7 +61,10 @@ export default function Sidebar() {
             <Link
               key={index}
               href={route.href}
-              className="group flex p-3 w-full justify-start text-sm font-medium hover:bg-accent hover:text-accent-foreground rounded transition duration-300 cursor-pointer"
+              className={cn([
+                "group flex p-3 w-full justify-start text-sm font-medium hover:bg-accent hover:text-accent-foreground rounded transition duration-300 cursor-pointer",
+                pathname === route.href && "bg-accent text-accent-foreground",
+              ])}
             >
               <div className="flex flex-1 items-center gap-4">
                 <route.icon className="h-4 w-4" />
